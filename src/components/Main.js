616f65fbe1152998f23574d34cdfa0f4e095b5cd
michaelprince3 <michaelprince3@gmail.com>
2020-08-05 14:31:24 -0500

    added files and routing

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import ApplicationViews from "./ApplicationViews";
+
+const Main = () => {
+
+    return (
+        <>
+        <ApplicationViews />
+        </>
+    )
+}
