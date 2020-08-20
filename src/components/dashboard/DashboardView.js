e93e28dcaeaafe648343353cbf270e5d6dbf8537
michaelprince3 <michaelprince3@gmail.com>
2020-08-19 21:06:28 -0500

    playing with video

--- src/components/dashboard/DashboardView.js
+++ src/components/dashboard/DashboardView.js
@@ -1,17 +1,27 @@
 import React, { useState, useEffect } from "react";
+import BackVideo from "/home/michaelprince3/workspace/react/home-dash/src/components/video/videoplayback.mp4"
+import "./DashboardView.css";
 
 const DashboardView = (props) => {
   const [weather, setWeather] = useState([]);
 
-
   return (
     <>
-    <div className="cardContainer">
-      
-    </div>
-    </>
-  )
+      <div className="backVideo">
+        <video autoPlay loop muted>
+          <source src={BackVideo} type="video/mp4"/>
+        </video>
+      </div>
+      <div className="cardContainer">
+        <div className="weatherCard">
+          <div className="cardContent">
+            <h1>Weather</h1>
 
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
-export default DashboardView
+export default DashboardView;
